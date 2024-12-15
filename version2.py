6d20e9f714eca2510aabf8ce8c8594b456ca2b48
Freddy Khant <105434008+freddykhant@users.noreply.github.com>
2024-12-15 19:33:03 +0800

    version 1 and 2 created

---
+++ version2.py
@@ -0,0 +1,34 @@
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+import streamlit as st
+
+# template = """
+# Answer the question below:
+
+# Here is the conversation history: {context}
+
+# Question: {question}
+
+# Answer:
+# """
+
+template = """
+Answer the question below:
+
+You are a helpful assistant, please answer any user queries to the best of your ability.
+
+Question: {question}
+
+Answer:
+"""
+
+st.title('Llama 3.1 Chatbot')
+input_text = st.text_input("Chat with Llama 3.1")
+
+model = OllamaLLM(model="llama3.1")
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+if input_text:
+    st.write(chain.invoke({"question": input_text}))
+
