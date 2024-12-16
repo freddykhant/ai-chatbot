d9c18737ca3e6849f0f75a1b8efd4e2d8857888b
Freddy Khant <105434008+freddykhant@users.noreply.github.com>
2024-12-16 14:37:08 +0800

    start of summarybot

---
+++ summarybot.py
@@ -0,0 +1,21 @@
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+
+template = """
+You are an expert data analyst. Given the following CSV data, generate a short summary or key insights in 50 words or less. Highlight any trends, anomalies, or interesting patterns in the data. Be clear, concise, and specific. Use plain language suitable for a general audience.
+
+Here is the data in csv format: {file}
+
+Summary:
+"""
+
+# model = OllamaLLM(model="llama3.1")
+# prompt = ChatPromptTemplate.from_template(template)
+# chain = prompt | model
+
+def summarise():
+    print("\nWelcome to Llama 3.1 for Data Analysis\n")
+    file_path = input("Please enter the file path of the data:\n")
+
+    with open(f"{file_path}", "r") as f:
+        print(f.read())
