735a1f52c7978555fa6be52c7441a0460b3d695f
Freddy Khant <freddyhmk@gmail.com>
2024-12-18 12:17:02 +0800

    Answer generation

--- RAG/RAGbot.py
+++ RAG/RAGbot.py
@@ -99,3 +99,27 @@ result = llm_json_mode.invoke(
     + [HumanMessage(content=doc_grader_prompt_formatted)]
 )
 json.loads(result.content)
+
+### Generate 
+
+# Prompt
+rag_prompt = """ You are an assistant for question-answering tasks.
+Here is the context to use to answer the question:
+{context}
+Think carefully about the above context.
+Now, review the user question:
+{question}
+Provide a answer to this question using only the above context.
+Use three sentences maximum and keep the answer concise.
+Answer:"""
+
+# Post-processing 
+def format_docs(docs):
+   return "\n\n".join(doc.page_content for doc in docs)
+
+# Test
+docs = retriever.invoke(question)
+docs_txt = format_docs(docs)
+rag_prompt_formatted = rag_prompt.format(context=docs_txt, question=question)
+generation = llm.invoke([HumanMessage(content=rag_prompt_formatted)])
+print(generation.content)
