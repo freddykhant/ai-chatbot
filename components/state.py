cdbdd3b32ce4cf645b362a29fc6b050263ec520e
Freddy Khant <freddyhmk@gmail.com>
2025-02-11 10:39:10 +0800

    set initial state

--- components/state.py
+++ components/state.py
@@ -1,5 +1,16 @@
 import streamlit as st
 
 def set_initial_state():
+  ### general ###
   if "embedding_model" not in st.session_state:
     st.session_state["embedding_model"] = "nomic-embed-text-v1.5"
+
+  ### RAG ###
+  if "llm" not in st.session_state:
+    st.session_state["llm"] = None
+
+  if "documents" not in st.session_state:
+    st.session_state["documents"] = []
+
+  if "retriever" not in st.session_state:
+    st.session_state["retriever"] = None
