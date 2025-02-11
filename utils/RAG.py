e13dbfdf5f635dd6722d0a59b6ed3c08242a96ab
Freddy Khant <freddyhmk@gmail.com>
2025-02-11 12:24:45 +0800

    move save file function to helper, add ui logging

--- utils/RAG.py
+++ utils/RAG.py
@@ -1,16 +1,17 @@
-from utils.setup import create_llm, create_embeddings
+from utils.setup import create_llm
 from utils.retriever import load_documents, create_retriever
+from utils.helper import save_uploaded_files
 import os
 import streamlit as st
 
 def rag_pipeline(uploaded_files: list=None):
   if uploaded_files is not None:
     for uploaded_file in uploaded_files:
-      save_dir = os.getcwd() + "/uploads"
-      if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-      with open(os.path.join(save_dir, uploaded_file.name), "wb") as f:
-        f.write(uploaded_file.getbuffer())
+      with st.spinner(f"Processing {uploaded_file.name}..."):
+        save_dir = os.getcwd() + "/uploads"
+        save_uploaded_files(uploaded_file, save_dir)
+
+    st.caption("Files Uploaded ✅")
 
   llm = create_llm()
   st.session_state["llm"] = llm
--- utils/helper.py
+++ utils/helper.py
@@ -1,3 +1,11 @@
+import os
+
 # method to format docs 
 def format_docs(docs):
     return "\n\n".join([doc.page_content for doc in docs])
+
+def save_uploaded_files(uploaded_file: bytes, save_dir: str):
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    with open(os.path.join(save_dir, uploaded_file.name), "wb") as f:
+        f.write(uploaded_file.getbuffer())
