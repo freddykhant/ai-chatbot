c2a9781fb0d5c961f275c868f150ae47a99c8ac1
Freddy Khant <freddyhmk@gmail.com>
2025-02-11 10:26:26 +0800

    remove hard coded variables

--- setup.py
+++ setup.py
@@ -3,11 +3,6 @@ from langchain_nomic.embeddings import NomicEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.prompts import PromptTemplate
 
-# variables
-# model = "llama3.1:8b"
-
-# emb_model = "nomic-embed-text-v1.5"
-
 chat_prompt = PromptTemplate.from_template(
   """
   You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question.
@@ -18,7 +13,6 @@ chat_prompt = PromptTemplate.from_template(
   Context: {context}
   """
 )
-
 
 # functions for setup
 def create_llm():
