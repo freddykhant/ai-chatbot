5cf5eb47bfd26e4cfa66daba26fb5e3abd59e1a4
Freddy Khant <freddyhmk@gmail.com>
2025-02-11 12:36:06 +0800

    added temp file removal

--- utils/RAG.py
+++ utils/RAG.py
@@ -2,9 +2,11 @@ from utils.setup import create_llm, create_embeddings
 from utils.retriever import load_documents, create_retriever
 from utils.helper import save_uploaded_files
 import os
+import shutil 
 import streamlit as st
 
 def rag_pipeline(uploaded_files: list=None):
+  # save files pipeline
   if uploaded_files is not None:
     for uploaded_file in uploaded_files:
       with st.spinner(f"Processing {uploaded_file.name}..."):
@@ -13,6 +15,7 @@ def rag_pipeline(uploaded_files: list=None):
 
     st.caption("Files Uploaded ✅")
 
+  # create LLM and embeddings
   llm = create_llm()
   st.session_state["llm"] = llm
   st.caption("Model loaded ✅")
@@ -20,6 +23,7 @@ def rag_pipeline(uploaded_files: list=None):
   embedding_model = st.session_state["embedding_model"]
   embeddings = create_embeddings(embedding_model)
 
+  # load documents
   if st.session_state["documents"] is not None and len(st.session_state["documents"]) > 0:
     st.caption("Documents Processed ✅")
   else:
@@ -28,4 +32,12 @@ def rag_pipeline(uploaded_files: list=None):
     st.session_state["documents"] = documents
     st.caption("Data Processed ✅")
 
+  # create retriever
   create_retriever(st.session_state["documents"], embeddings)
+  st.caption("Retriever Created ✅")
+
+  # remove temp files
+  if len(st.session_state["files"]) > 0:
+    save_dir = os.getcwd() + "/uploads"
+    shutil.rmtree(save_dir)
+    st.caption("Removed Temp Files ✅")
