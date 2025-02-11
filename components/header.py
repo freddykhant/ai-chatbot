b27dcff6602212572f0aadd4c5817c9079f26079
Freddy Khant <freddyhmk@gmail.com>
2025-02-11 10:42:28 +0800

    header for page

---
+++ components/header.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def set_page_header():
+  st.header("🦙LLama 3.1 Chatbot", anchor=False)
+  st.caption("A conversational AI model that can answer questions and ingest data for retrieval augmented generation (RAG)")
