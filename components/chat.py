1bd9c622418c5aa42ca3895a3dd13d574e39bbf0
Freddy Khant <freddyhmk@gmail.com>
2025-02-19 09:37:02 +0800

    remove requirement to upload file first

--- components/chat.py
+++ components/chat.py
@@ -3,10 +3,6 @@ from utils.setup import chat_context, chat, create_llm
 
 def chatbot():
   if prompt := st.chat_input("How can I help?"):
-    # if not st.session_state["retriever"]:
-    #   st.error("Please upload a document first")
-    #   st.stop()
-
     st.session_state["messages"].append({"role": "user", "content": prompt})
     with st.chat_message("user"):
       st.markdown(prompt)
--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from components.chat import chatbot
 from components.sidebar import sidebar
 from components.header import set_page_header
 from components.state import set_initial_state
+import os 
+
+os.environ["STREAMLIT_WATCH_DISABLE"] = "true"
+os.environ["USER_AGENT"] = "Mozilla/5.0 (compatible; MyStreamlitApp/1.0)"
 
 set_initial_state()
 set_page_header()
